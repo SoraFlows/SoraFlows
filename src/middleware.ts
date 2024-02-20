94b6b7592fa5d1cb3d63ecb14608a2387f67cef2
RongkangXiong <earth@mail.ustc.edu.cn>
2024-02-21 00:58:18 +0800

    feat: except /v1 route

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,6 +28,10 @@ function getLocale(request: NextRequest): string | undefined {
 export function middleware(request: NextRequest) {
     const pathname = request.nextUrl.pathname;
 
+    if (pathname.startsWith('/v1')) {
+        return NextResponse.next();
+    }
+
     // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
     // // If you have one
     // if (
@@ -62,5 +66,5 @@ export function middleware(request: NextRequest) {
 
 export const config = {
     // Matcher ignoring `/_next/` and `/api/`
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|v1).*)"],
 };
