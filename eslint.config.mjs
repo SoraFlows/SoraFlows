7ea2d2a3c28e22f0ff887828edf586963900d144
wang kaiyu <wangkaiyu@youzhicai.com>
2024-02-20 20:10:09 +0800

    build: add prettier and specify npm registry

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+// eslint.config.mjs
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+    // 自定义配置
+    react: true,
+})
--- src/app/[lang]/Header.tsx
+++ src/app/[lang]/Header.tsx
@@ -1,22 +1,48 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import React from "react";
 import Head from "next/head";
 
 export const LayoutHeader = () => {
-    return (
-        <header className="fixed left-0 right-0 top-0 z-[19] border-b-[0.5px] border-zinc-200 bg-white/80 pl-6 backdrop-blur">
-            <nav className="flex h-16 justify-between items-center">
-                <div>
-                <Link href="/" className="font-bold opacity-90 md:text-base mr-4 lg:mr-8">Go Home</Link>
-                </div>
-                <div className='flex'>
-                    <Link href="/" className="hidden lg:flex font-bold opacity-90 md:text-base px-4">Contact Us</Link>
-                    <Link href="/" className="hidden lg:flex font-bold opacity-90 md:text-base px-4">Home</Link>
-                    <Link href="/" className="hidden lg:flex font-bold opacity-90 md:text-base px-4">About us</Link>
-                </div>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header className="fixed left-0 right-0 top-0 z-[19] border-b-[0.5px] border-zinc-200 bg-white/80 pl-6 backdrop-blur">
+      <nav className="flex h-16 justify-between items-center">
+        <div>
+          <Link
+            href="/"
+            className="font-bold opacity-90 md:text-base mr-4 lg:mr-8"
+          >
+            Go Home
+          </Link>
+        </div>
+        <div className="flex">
+          <Link
+            href="/"
+            className="hidden lg:flex font-bold opacity-90 md:text-base px-4"
+          >
+            Contact Us
+          </Link>
+          <Link
+            href="/"
+            className="hidden lg:flex font-bold opacity-90 md:text-base px-4"
+          >
+            Home
+          </Link>
+          <Link
+            href="/"
+            className="hidden lg:flex font-bold opacity-90 md:text-base px-4"
+          >
+            About us
+          </Link>
+          <Link
+            href="/"
+            className="hidden lg:flex font-bold opacity-90 md:text-base px-4"
+          >
+            About us
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
