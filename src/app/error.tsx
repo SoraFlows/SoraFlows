6d52665ced06546de9a1d8086e74c962dd6ad04d
RongkangXiong <earth@mail.ustc.edu.cn>
2024-02-21 02:32:05 +0800

    feat: add error not-found

---
+++ src/app/error.tsx
@@ -0,0 +1,18 @@
+'use client';
+import {useEffect} from 'react';
+
+type Props = {
+  error: Error;
+  reset(): void;
+};
+
+export default function Error({error, reset}: Props) {
+
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>not found</div>
+  );
+}
