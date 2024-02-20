29be740f918d6c90247a2fa94154c01422f89dc0
RongkangXiong <earth@mail.ustc.edu.cn>
2024-02-21 03:28:44 +0800

    Update layout.tsx

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,17 +1,17 @@
-import {Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 
 export default function RootLayout({
-                                       children
-                                   }: Readonly<{
+    children
+}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <html lang="en">
-        <body className={inter.className}>{children}</body>
+            <body className={inter.className}>{children}</body>
         </html>
     );
 }
