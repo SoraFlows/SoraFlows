d10c3cb70da7dd196edb63284d601a997bf78eb6
RongkangXiong <earth@mail.ustc.edu.cn>
2024-02-22 20:28:51 +0800

    feat: create page

---
+++ src/app/[lang]/(video)/submit-video/page.tsx
@@ -0,0 +1,9 @@
+// This page only renders when the app is built statically (output: 'export')
+export default function Submit() {
+    return (
+       <div>
+          Submit
+       </div>
+    );
+   }
+   
---
+++ src/app/[lang]/(video)/video-showcase/page.tsx
@@ -0,0 +1,9 @@
+// This page only renders when the app is built statically (output: 'export')
+export default function VideoShowCase() {
+    return (
+       <div>
+          VideoShowCase
+       </div>
+    );
+   }
+   
