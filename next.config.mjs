4e03b561d62e0c0ebebb2ba44f4122561e2f2870
RongkangXiong <earth@mail.ustc.edu.cn>
2024-02-26 22:43:47 +0800

    fix: 图片优化关了

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    
     images: {
+        unoptimized:true,
         domains: ['www.favicon.vip'], // 添加你需要的外部图片域名
         remotePatterns: [{
             protocol: 'https',
