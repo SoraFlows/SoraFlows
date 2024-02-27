ddeda572400cd53d654ed5746c5b331a6383dc20
RongkangXiong <earth@mail.ustc.edu.cn>
2024-02-28 00:55:47 +0800

    fix: videos url error

--- src/components/Menu/PCTopMenu.tsx
+++ src/components/Menu/PCTopMenu.tsx
@@ -15,7 +15,7 @@ export default function PCTopMenu() {
                     Studio
                 </Link>
                 <Link
-                    href="/vieos"
+                    href="/videos"
                     className="lg:flex font-bold opacity-90 md:text-base px-4 py-2 pr-4 hover:bg-gray-200 rounded-xl transition duration-300"
                 >
                     Videos
